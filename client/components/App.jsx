7404b43d90dfcfd3b36b8c6b3625cdc419c36f51
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2022-09-07 15:34:59 +1200

    css started

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,11 +16,20 @@ function App() {
       <div className="app">
         <h1>Characters of the Destiny Universe</h1>
         <Loading />
-        <ul>
-          {characters.map((character) => (
-            <li key={character.id}>{character.name}</li>
-          ))}
-        </ul>
+        <div className="character-container">
+          {characters.map((character) => {
+            return (
+              <div className="character-box" key={character.id}>
+                <div className="character-info">
+                  <p>{character.name}</p>
+                  <img src={`/images/${character.image}`} />
+                  <p>{character.class}</p>
+                  <p>{character.bio}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
